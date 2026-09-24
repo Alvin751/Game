cf61616a0d41bb0a40592e6fa62612752530851c
Alvin :) <94037513+Alvin751@users.noreply.github.com>
2026-09-25 00:00:16 +0700

    Game screen background update code
    update

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-#Since I set up the Github account, I will just add some game ideas here:
-#Platformer with a player and obstacles that the player has to jump over, and you can play the game to get a high score.
-#You could use space bar to jump or maybe arrow keys.
-#Idk who the character would be or the theme of the game yet, probably space related.
-#Also what should the game sprites use: bitmap or vector.
-
+#This is the game screen code with a custom background.
 import tkinter as tk
 root = tk.Tk()
+root.geometry("500x500")
+
+root.configure(bg="#93DEFF")
 root.mainloop()
