79b4c4f831019b138a995044f4e0eb4349a2ce69
Alvin :) <94037513+Alvin751@users.noreply.github.com>
2026-09-24 23:40:40 +0700

    Game ideas
    update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-print("Hello")
-#Python
-#Testing that you can see the progress when uploading the main.py file.
-#Test again
-#Okay I think that I can just update this page whenever I write new code, and press the "Commit changes" button.
+#Since I set up the Github account, I will just add some game ideas here:
+#Platformer with a player and obstacles that the player has to jump over, and you can play the game to get a high score.
+#You could use space bar to jump or maybe arrow keys.
+#Idk who the character would be or the theme of the game yet, probably space related.
+#Also what should the game sprites use: bitmap or vector.
